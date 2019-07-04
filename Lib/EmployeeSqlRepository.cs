017e720cf9e70a49123b0eac885d5e7ca4065990
Assad Faizi <Assad.Faizi@neilsonfs.com>
2019-07-04 10:00:26 +0100

    Basic Database App

---
+++ Lib/EmployeeSqlRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace Lib
+{
+    public class EmployeeSqlRepository
+    {
+        private readonly Func<Task<IDbConnection>> _dbConnectionFactory;
+
+        public EmployeeSqlRepository(Func<Task<IDbConnection>> dbConnectionFactory)
+        {
+            _dbConnectionFactory = dbConnectionFactory;
+        }
+
+        public async Task<Employee> GetEmployeeAsync(int id)
+        {
+            using (var dbConnection = await _dbConnectionFactory().ConfigureAwait(false))
+            {
+                await dbConnection.ExecuteAsync("THROW 50001, 'Oops Error Occurred!!!', 1").ConfigureAwait(false);
+                var employee = await dbConnection.QueryFirstOrDefaultAsync<Employee>("SELECT 1 as ID, 'Joe' as FirstName, 'Blog' as LastName, 100 as DepartmentID").ConfigureAwait(false);
+
+                return employee;
+            }
+        }
+    }
+}
---
+++ ReliablePollyDbApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Lib;
+using Newtonsoft.Json;
+
+namespace ReliablePollyDbApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Started...");
+
+            try
+            {
+                MainAsync(args).GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+            Console.WriteLine("Finished!");
+            Console.ReadLine();
+        }
+
+        public static async Task MainAsync(string[] args)
+        {
+            var repo = new EmployeeSqlRepository(async () =>
+            {
+                var conn = new SqlConnection("Server=localhost;Database=Sandpit;Trusted_Connection=True;");
+                await conn.OpenAsync().ConfigureAwait(false);
+
+                return conn;
+            });
+
+            var employee = await repo.GetEmployeeAsync(1).ConfigureAwait(false);
+
+            Console.WriteLine(JsonConvert.SerializeObject(employee));
+        }
+    }
+}
